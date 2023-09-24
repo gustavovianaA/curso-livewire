0834f921c74b86832c9800ee290f7080bcedb0cd
Gustavo <gustavow3dev@gmail.com>
2023-09-24 14:55:29 -0300

    aula 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Curso Laravel Livewire</title>
-    @livewireStyles
-</head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-<body>
-    <div class="container">
-        {{ $slot }}
-    </div>
-    @livewireScripts
-</body>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        @livewireStyles
+       
+        <!-- Scripts -->
+        
+    </head>
+    <body class="font-sans antialiased">
+        <x-jet-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tweets' , ShowTweets::class);
+Route::get('tweets' , ShowTweets::class)->middleware('auth');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
